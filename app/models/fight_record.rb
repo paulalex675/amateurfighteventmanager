c1fbb0727e4b45e85ee2504c372f307a67d04471
Paul Robinson <paulalex675@hotmail.com>
2021-06-07 09:34:55 +0100

    latest updates for clone

--- app/models/fight_record.rb
+++ app/models/fight_record.rb
@@ -1,3 +1,11 @@
 class FightRecord < ApplicationRecord
   belongs_to :user, class_name: "User", foreign_key: "user_id"
+
+  def total_fights
+    self.win + self.draw + self.lose
+  end
+
+  def win_percentage
+    (self.win/self.total_fights.to_f) * 100
+  end
 end
